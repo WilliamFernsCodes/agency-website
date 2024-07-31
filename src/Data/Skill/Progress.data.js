fc6398c080ed26ff989714d6f5f5e5db86f22239
WilliamFernsCodes <williamfernscodes@gmail.com>
2024-07-31 12:37:57 +0200

    feat(Progress): changed data

--- src/Data/Skill/Progress.data.js
+++ src/Data/Skill/Progress.data.js
@@ -1,18 +1,14 @@
 export default [
-    {
-        field: "Graphics Design",
-        progress: 70
-    },
-    {
-        field: "Ui/Ux Design",
-        progress: 80
-    },
-    {
-        field: "Web Development",
-        progress: 90
-    },
-    {
-        field: "App Development",
-        progress: 85
-    }
-]
+  {
+    field: "Web Development",
+    progress: 90,
+  },
+  {
+    field: "Backend Development",
+    progress: 90,
+  },
+  {
+    field: "UI/UX Design",
+    progress: 80,
+  },
+];
