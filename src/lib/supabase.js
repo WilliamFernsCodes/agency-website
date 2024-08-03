e5fe28eaa01fea1ace862d601ff353cbd0e1d827
WilliamFernsCodes <williamfernscodes@gmail.com>
2024-08-03 09:04:53 +0200

    feat(supabase): added 30+ designs projects card at end of data

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -8,15 +8,25 @@ const getProjects = async () => {
   if (error) {
     throw new Error(error.message);
   }
-  return data.map((project) => {
-    return {
-      projectName: project.project_name,
-      projectType: project.project_type,
-      imagePath: `${process.env.NEXT_PUBLIC_SUPABASE_BASE_URL}/storage/v1/object/public/${project.image_path}`,
-      tags: project.tags,
-      url: project.url,
-    };
-  });
+  const projects = [
+    ...data.map((project) => {
+      return {
+        projectName: project.project_name,
+        projectType: project.project_type,
+        imagePath: `${process.env.NEXT_PUBLIC_SUPABASE_BASE_URL}/storage/v1/object/public/${project.image_path}`,
+        tags: project.tags,
+        url: project.url,
+      };
+    }),
+    {
+      projectName: "See More",
+      projectType: "ui",
+      imagePath: `${process.env.NEXT_PUBLIC_SUPABASE_BASE_URL}/storage/v1/object/public/images/all_ui_ux_projects.png`,
+      url: "https://www.figma.com/design/OlyX6mdzVhTlZRS2MV76Aa/Muhammad-Shyed-Hassain-(Collaborator-Designer)?node-id=0-1&t=myPwzWd5qmRjswb8-1",
+      tags: ["30+ designs", "figma"],
+    },
+  ];
+  return projects;
 };
 
 export { getProjects };
